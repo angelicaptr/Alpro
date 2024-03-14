3d9df9cd00317391c75772c0a0b27dbb42f8fb49
angelica <163296716+satanlica@users.noreply.github.com>
2024-03-14 18:03:44 +0700

    SegitigaUnik.c

--- Prak. Alpro/SegitigaUnik.c
+++ Prak. Alpro/SegitigaUnik.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    int n;
+int main(){
+    int i, j, n, count;
 
-    printf("Masukkan nilai n=")
+    printf("Masukkan n : ");
     scanf("%d", &n);
-    for (int i=0; i<n; i++)
-    if 
 
-    return 0;
+    count = 0;
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= i; j++){
+            printf("%d", count % 10);
+            count ++;
+        }
+        printf("\n");
+
+    }
+    return 0;
 }
