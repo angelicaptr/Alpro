d549bfc3a068c2f7c04ebb5fc2aa87d8f0fec98a
angelica <163296716+satanlica@users.noreply.github.com>
2024-03-14 17:06:57 +0700

    Update jarakPBola.c

--- Prak. Alpro/jarakPBola.c
+++ Prak. Alpro/jarakPBola.c
@@ -6,12 +6,13 @@ int main () {
     float g = 9.8;
 
     printf("Masukkan kecepatan awal (v0): ");
-    scanf("d%", &v0);
+    scanf("%d", &v0);
     printf("Masukkan waktu (t): ");
-    scanf("d%", &t);
+    scanf("%d", &t);
     y = v0 * t - 0.5 * (g * t * t);
 
-    printf("Jarak yang ditempuh: %f meter/n", y);
+    printf("Jarak yang ditempuh: %f meter\n", y);
 
     return 0;
-    }
+    
+}
