7abc019b6b691ab8011ad2b0eee66abd7163c28b
angelica <163296716+satanlica@users.noreply.github.com>
2024-03-14 16:27:12 +0700

    AkagiKongou.c

--- Prak. Alpro/AkagiKongou.c
+++ Prak. Alpro/AkagiKongou.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    int A,B,C;
+int main(){
+	
+	int A, B, C, babak = 0;
 
-    printf("Masukkan nilai A=")
-    scanf ("%d", &A);
-    printf("Masukkan nilai B=")
-    scanf ("%d", &B);
-    printf ("Masukkan nilai C=")
-    scanf ("%d", &C);
-
-    
+	printf("Masukkan angka yang dipilih Akagi :");
+	scanf("%d", &A);
+	printf("Masukkan angka yang dipilih Kongou :");
+	scanf("%d",&B);
+	
+	while (B > 0){
+		babak ++;
+		printf("babak :%d\n",babak);
+		C = A - B;	
+		printf ("C = %d\n", C);
+		A = B;
+		printf ("A = %d\n", A);
+		B = C;
+		printf ("B = %d\n", B);
+	}
+	
+	if (B < 0){
+		printf ("Pemenangnya adalah Akagi");
+	}
+	else {
+		printf("Pemenangnya adalah Kongou");
+	}
+	return 0;
 }
